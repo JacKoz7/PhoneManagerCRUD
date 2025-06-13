af0e14bf641becef0d5ac158a7cf26c097b9e6cd
Jacek <jacuskozlowski@gmail.com>
2025-06-13 09:50:15 +0200

    feat(frontend): configure routing for phone management

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PhoneListComponent } from './components/phone-list/phone-list';
+import { PhoneFormComponent } from './components/phone-form/phone-form';
+import { PhoneDetailComponent } from './components/phone-detail/phone-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'phones', pathMatch: 'full' },
+  { path: 'phones', component: PhoneListComponent },
+  { path: 'phones/new', component: PhoneFormComponent },
+  { path: 'phones/:id', component: PhoneDetailComponent },
+  { path: 'phones/:id/edit', component: PhoneFormComponent },
+  { path: '**', redirectTo: 'phones' }
+];

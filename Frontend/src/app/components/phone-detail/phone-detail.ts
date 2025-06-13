fe8b82275a9f7bc9b602fe7d5a24a09b6e2eaeb2
Jacek <jacuskozlowski@gmail.com>
2025-06-13 09:10:40 +0200

    feat: initial project setup

---
+++ Frontend/src/app/components/phone-detail/phone-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-detail',
+  imports: [],
+  templateUrl: './phone-detail.html',
+  styleUrl: './phone-detail.css'
+})
+export class PhoneDetail {
+
+}
---
+++ Frontend/src/app/components/phone-list/phone-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-list',
+  imports: [],
+  templateUrl: './phone-list.html',
+  styleUrl: './phone-list.css'
+})
+export class PhoneList {
+
+}
